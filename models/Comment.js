7df8cca864ec6aec90b521c710018ad694b4970b
Hussain <hussain.alsayedali@gmail.com>
2023-03-05 17:30:17 +0300

    foundation layed

---
+++ models/Comment.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user", 
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
+
+  score: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
