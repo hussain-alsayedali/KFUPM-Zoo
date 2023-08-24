6ec5651fcfc834578a9e199423112bd42d0167ab
Hussain <hussain.alsayedali@gmail.com>
2023-08-24 09:57:37 +0300

    removed routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/multer");
 const postsController = require("../controllers/posts");
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const { ensureAuth, ensureGuest,isAuthed } = require("../middleware/auth");
 
 //Post Routes - simplified for now
-router.get("/:id", postsController.getPost);
+router.get("/:id", isAuthed ,postsController.getPost);
 router.get("/getPostComments/:id", postsController.getPostComments);
 
 router.post("/createPost",ensureAuth, upload.single("file"), postsController.createPost);
