45c5da617a21ed239cd0e69ef5c6b64a4df90e0a
Hussain <hussain.alsayedali@gmail.com>
2023-08-25 15:30:45 +0300

    css purged(minified)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./views/*.{html,js,ejs}",
-  './views/partials/*.{html,js,ejs}'
-  ],
-  safelist: [
-    {
-      pattern: /./, // the "." means "everything"
-    },
-  ],
+  './views/partials/*.{html,js,ejs}',
+  "./public/javascript/*"
+  ]
+  ,
+  // safelist: [
+  //   {
+  //     pattern: /./, // the "." means "everything"
+  //   },
+  // ],
 
   theme: {
     extend: {},
